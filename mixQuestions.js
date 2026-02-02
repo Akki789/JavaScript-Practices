252fc46330a2da8262cee28e6b086199579eb4fb
Akash <aakashkumarag96@gmail.com>
2026-02-02 21:12:00 +0530

    Practiced some mix question of TDZ, hoisting and all

---
+++ mixQuestions.js
@@ -0,0 +1,39 @@
+//Question 1
+// console.log(typeof a); // undefined
+// let a = 10;
+
+//question 2
+// var a = 1;
+
+// (function () {
+//   console.log(a); //undefined TDZ
+//   let a = 2;
+// })();
+
+//question 3
+    // function test() {
+    //   console.log(a);
+    // }
+
+    // test(); // undefined TDZ
+
+    // let a = 10;
+
+//question 4
+// console.log(a); //undefined TDZ
+
+// const a = 10;
+
+//Question 5
+// var a = 10;
+
+// function outer() {
+//   console.log(a); 
+//   var a = 20;
+// }
+
+// outer(); //undefined
+
+//Question 6
+
+
